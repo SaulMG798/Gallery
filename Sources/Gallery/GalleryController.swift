967c950dd7dbb2445a458b9bef68eeb84b7653b0
SaulMG798 <saul.marquez@appconecte.com>
2018-08-31 17:54:19 -0500

    Se agrego el controlador de videos

--- Sources/Gallery/GalleryController.swift
+++ Sources/Gallery/GalleryController.swift
@@ -78,24 +78,15 @@ public class GalleryController: UIViewController, PermissionControllerDelegate {
 
     let controllers: [UIViewController] = tabsToShow.compactMap { tab in
         
-        if tab == .imageTab {
-            return makeImagesController()
-        } else if tab == .cameraTab {
-            return makeCameraController()
-        } else {
-            return nil
-        }
-        
-        
-//      if tab == .imageTab {
-//        return makeImagesController()
-//      } else if tab == .cameraTab {
-//        return makeCameraController()
-//      } else if tab == .videoTab {
-//        return makeVideosController()
-//      } else {
-//        return nil
-//      }
+      if tab == .imageTab {
+        return makeImagesController()
+      } else if tab == .cameraTab {
+        return makeCameraController()
+      } else if tab == .videoTab {
+        return makeVideosController()
+      } else {
+        return nil
+      }
     }
 
     guard !controllers.isEmpty else {
