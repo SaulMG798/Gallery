f0a645c1e042ab2cdc1ef2f468644e5a634b6364
SaulMG798 <saul.marquez@appconecte.com>
2018-08-31 18:53:35 -0500

    correccion titulo

--- Sources/Videos/VideosController.swift
+++ Sources/Videos/VideosController.swift
@@ -60,7 +60,7 @@ class VideosController: UIViewController {
     gridView.collectionView.delegate = self
     gridView.collectionView.register(VideoCell.self, forCellWithReuseIdentifier: String(describing: VideoCell.self))
 
-    gridView.arrowButton.updateText("Gallery.AllVideos".g_localize(fallback: "ALL VIDEOS"))
+    gridView.arrowButton.updateText("Gallery.AllVideos".g_localize(fallback: "VIDEOS"))
     gridView.arrowButton.arrow.isHidden = true
   }
 
